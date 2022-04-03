6c6885dce4dffb1652e2e1a9da7e470cae6e906d
Chris Snyder <csnyder332@gmail.com>
2022-04-03 12:14:26 -0500

    working page

--- models/index.js
+++ models/index.js
@@ -1,11 +1,35 @@
 const User = require('./User');
-const Posts = require('./Posts');
-User.hasMany(Posts, {
-  foreignKey: 'user_id',
-});
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-Posts.belongsTo(User, {
-  foreignKey: 'user_id',
-});
 
-module.exports = { User, Posts};
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+})
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+})
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
+
+
+module.exports = {
+    User,
+    Post,
+    Comment
+};
