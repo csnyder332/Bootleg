ba8bbd110aa8f127a1f9110137e7d090e5746c55
Alec Harvey <ruinhard@gmail.com>
2022-04-03 11:14:32 -0500

    deleted timestamp from model and created seed folder.
    timestamps arent necessary because they are already add

--- controllers/dir-routes.js
+++ controllers/dir-routes.js
@@ -4,5 +4,12 @@ const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
       res.render('homepage');
-  });
+});
+
+router.get('/login',(req,res)=>{
+    if (req.session.loggedIn){
+        res.redirect("/")
+    }
+    res.render("login")
+})
   module.exports=router
