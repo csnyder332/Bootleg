3ed8d70bc2e57e391fbd8f287906343a2b1850bd
Alec Harvey <ruinhard@gmail.com>
2022-04-03 14:31:11 -0500

    created model for replies

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const postRoutes=require("./post-routes")
+const replyRoutes = require("./reply-routes")
 
 router.use('/users', userRoutes);
+router.use("/posts",postRoutes)
+router.use("/reply",replyRoutes)
 
 module.exports = router;
