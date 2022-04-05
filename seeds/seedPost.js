643b44438f7d0edaef6b46f0ae7674ba33af7d9d
Alec Harvey <ruinhard@gmail.com>
2022-04-05 13:39:54 -0500

    displaying seeded images now works

--- seeds/seedPost.js
+++ seeds/seedPost.js
@@ -2,7 +2,7 @@ const { posts } = require('../models');
 
 const postdata = [
   {
-    image_url: '3c61407b-9bdf-4791-bd07-cb11deb97806',
+    image_url: '3c61407b-9bdf-4791-bd07-cb11deb97806.png',
     caption:'This is me, programming',
     user_id: 1
   }
