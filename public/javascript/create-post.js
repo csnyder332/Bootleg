5fbb1ad55319be84e887252f9687d63b61087ab7
Alec Harvey <ruinhard@gmail.com>
2022-04-04 20:55:03 -0500

    fixed dashboard and start process of uploading images

--- public/javascript/create-post.js
+++ public/javascript/create-post.js
@@ -1,9 +1,25 @@
 
 async function createPostHandler(event) {
     event.preventDefault();
+    console.log(event.target);
 
-    document.location.replace('/dashboard/new')
+    const response = await fetch(`/api/posts`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            post_content
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(response.statusText);
+    }
 }
 
 
-document.querySelector('#create-new-post').addEventListener('click', createPostHandler);
+document.querySelector('#new-post-form').addEventListener('submit', createPostHandler);
