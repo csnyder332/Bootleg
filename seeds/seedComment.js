af5f7200d74b600acebbb7e23b3d83657f97d38a
Alec Harvey <ruinhard@gmail.com>
2022-04-05 20:31:18 -0500

    users can now commentate on posts.

--- seeds/seedComment.js
+++ seeds/seedComment.js
@@ -5,6 +5,11 @@ const commentData = [
         content:"Thats definetly SOMETHING programming",
         user_id:2,
         post_id:1
+    },
+    {
+        content:"lame meme dude",
+        user_id:1,
+        post_id:1
     }
 ]
 const seedComment= ()=>comment.bulkCreate(commentData)
