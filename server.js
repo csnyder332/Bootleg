36d629c83e2a500dd5f33b34dfa8b1c9f0d6a149
Chris Snyder <csnyder332@gmail.com>
2022-04-08 12:24:11 -0500

    images folder

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(session(sess));
-app.use(fileUpload())
+app.use(fileUpload(path.join('images')));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, "images")));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(routes);
